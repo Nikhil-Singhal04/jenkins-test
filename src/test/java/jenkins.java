5d6188b95b07d9a85a2241c6d5906ea082c3d299
Nikhil Singhal <nikhilsinghal2019@gmail.com>
2026-06-18 00:12:21 +0530

    added

--- src/test/java/jenkins.java
+++ src/test/java/jenkins.java
@@ -1,21 +1,27 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class jenkins {
 
     @Test
-    public void testLogin() {
+    public void googleTitleTest() {
 
-        WebDriver driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
 
-        driver.get("https://google.com");
+        WebDriver driver = new ChromeDriver(options);
 
-        System.out.println(driver.getTitle());
+        driver.get("https://www.google.com");
 
-        // Force failure for demo
-        Assert.assertEquals(driver.getTitle(), "Google");
+        String title = driver.getTitle();
+        System.out.println("Page Title: " + title);
+
+        Assert.assertTrue(title.contains("Google"));
 
         driver.quit();
     }
