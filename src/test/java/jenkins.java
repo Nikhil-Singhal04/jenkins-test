eff2f829cdedfc6fc3ca1fc2ecfdedde255d9184
Nikhil Singhal <nikhilsinghal2019@gmail.com>
2026-06-17 23:54:55 +0530

    added

--- src/test/java/jenkins.java
+++ src/test/java/jenkins.java
@@ -1,12 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class jenkins {
 
     @Test
-    public void test() {
+    public void testLogin() {
 
-        Assert.assertEquals(1, 2);
+        WebDriver driver = new ChromeDriver();
 
+        driver.get("https://google.com");
+
+        System.out.println(driver.getTitle());
+
+        // Force failure for demo
+        Assert.assertEquals(driver.getTitle(), "Facebook");
+
+        driver.quit();
     }
 }
